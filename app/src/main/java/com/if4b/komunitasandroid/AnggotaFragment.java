c07f3c7d7f0e865e80b735ded95172b850574155
RelinPramudiya <relinrp@mhs.mdp.ac.id>
2022-05-11 21:03:02 +0700

    commit
    Fragment update

--- app/src/main/java/com/if4b/komunitasandroid/AnggotaFragment.java
+++ app/src/main/java/com/if4b/komunitasandroid/AnggotaFragment.java
@@ -12,10 +12,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link BerandaFragment#newInstance} factory method to
+ * Use the {@link AnggotaFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BerandaFragment extends Fragment {
+public class AnggotaFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class BerandaFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public BerandaFragment() {
+    public AnggotaFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class BerandaFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment BerandaFragment.
+     * @return A new instance of fragment AnggotaFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static BerandaFragment newInstance(String param1, String param2) {
-        BerandaFragment fragment = new BerandaFragment();
+    public static AnggotaFragment newInstance(String param1, String param2) {
+        AnggotaFragment fragment = new AnggotaFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,12 +61,12 @@ public class BerandaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beranda, container, false);
+        return inflater.inflate(R.layout.fragment_anggota, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        getActivity().setTitle("Beranda");
+        getActivity().setTitle("Anggota");
     }
 }
--- app/src/main/java/com/if4b/komunitasandroid/MainActivity.java
+++ app/src/main/java/com/if4b/komunitasandroid/MainActivity.java
@@ -1,14 +1,38 @@
 package com.if4b.komunitasandroid;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+
+    Fragment fragment;
+    FragmentTransaction transaction;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView = findViewById(R.id.navigation_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        toggle.syncState(); 
     }
 }
